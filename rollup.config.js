aa0fa4efcb4bb4bf9c564df0941fedc949e4f1c1
heyMP <mgp140@gmail.com>
2022-09-02 10:54:19 -0400

    override pfe-icon version

--- rollup.config.js
+++ rollup.config.js
@@ -5,6 +5,8 @@ import resolve from '@rollup/plugin-node-resolve';
 import {terser} from 'rollup-plugin-terser';
 import minifyHTML from 'rollup-plugin-minify-html-literals';
 import summary from 'rollup-plugin-summary';
+import css from 'rollup-plugin-import-css';
+import del from 'rollup-plugin-delete';
 
 export default {
   plugins: [
@@ -19,23 +21,29 @@ export default {
       warnings: true,
     }),
     // Print bundle summary
+    css(),
     summary(),
     // // Optional: copy any static assets to build directory
     // copy({
     //   patterns: ['images/**/*'],
     // }),
+    del({
+      targets: 'dist/*'
+    }),
   ],
   input: {
     'rh-footer': './node_modules/@rhds/elements/elements/rh-footer/rh-footer.js',
-    'pfe-cta': './node_modules/@patternfly/pfe-cta/dist/pfe-cta.js',
+    'rh-cta': './node_modules/@rhds/elements/elements/rh-cta/rh-cta.js',
+    'pfe-navigation': './node_modules/@cpelements/pfe-navigation/dist/pfe-navigation.js',
+    'lightdom': './lightdom.css.js',
   },
   output: [{
-    dir: './dist/js/modules',
+    dir: './dist',
     // Manually set the entry file name so we can load it in Drupal.
-    entryFileNames: '[name]-entry.js',
+    entryFileNames: '[name].js',
     // Each of these chunks are imported from the entry bundle (and children)
     // so we don't have to manually add these to Drupal.
-    chunkFileNames: '[name]-[hash].js',
+    chunkFileNames: '[name].js',
     format: 'esm'
   }],
   preserveEntrySignatures: 'strict',

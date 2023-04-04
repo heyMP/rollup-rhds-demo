3130008fb3db7f2822a0a1e0fef4b3a7344307a9
Michael Potter <mgp140@gmail.com>
2023-04-04 09:59:22 -0400

    fix: copying pf-icons to dist

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,12 @@
 // import typescript from '@rollup/plugin-typescript';
 // Import rollup plugins
-import {copy} from '@web/rollup-plugin-copy';
 import resolve from '@rollup/plugin-node-resolve';
 import {terser} from 'rollup-plugin-terser';
 import summary from 'rollup-plugin-summary';
 import css from 'rollup-plugin-import-css';
 import del from 'rollup-plugin-delete';
-import path from 'path';
+import copy from 'rollup-plugin-copy'
+
 
 export default {
   plugins: [
@@ -26,22 +26,14 @@ export default {
     del({
       targets: 'dist/*'
     }),
-    /* @todo copy pf-icons out of node_modules */
-    /* copy({ */
-    /*   targets: [ */
-    /*     { src: path.resolve(__dirname, 'node_modules/@patternfly/elements/pf-icon/icons/fab/alipay.js'), dest: path.join('dist') } */
-    /*   ] */
-    /* }), */
+    copy({
+      targets: [
+        { src: `node_modules/@patternfly/elements/pf-icon/icons`, dest: 'dist' }
+      ]
+    }),
   ],
   // Single bundle example
   input: 'bundle.js',
-  // // ESM module based dist directory example
-  // input: {
-  //   'rh-footer': './node_modules/@rhds/elements/elements/rh-footer/rh-footer.js',
-  //   'rh-cta': './node_modules/@rhds/elements/elements/rh-cta/rh-cta.js',
-  //   'pfe-navigation': './node_modules/@cpelements/pfe-navigation/dist/pfe-navigation.js',
-  //   'lightdom': './lightdom.css.js',
-  // },
   output: [{
     dir: './dist',
     entryFileNames: '[name].js',
